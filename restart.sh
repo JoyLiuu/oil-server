3fe6af859def38a60c867887d14005fe6fff2375
lufei <lxyndy@163.com>
2026-09-14 23:45:03 +0800

    0914

--- restart.sh
+++ restart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /opt/docker/oil-server
-docker build -t wx-oil-server .
 docker compose down
 docker rmi wx-oil-server
+docker build -t wx-oil-server .
 docker compose up -d
 docker logs -f wx-oil-server
